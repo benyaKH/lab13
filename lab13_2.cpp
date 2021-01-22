7cb9b63703107679ccab99f1133e99d542667d56
Kaofang <benyapa_kh@cmu.ac.th>
2021-01-22 17:02:54 +0700

    upd_matrix

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -19,3 +19,42 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool matrix[][N]){
+    for(int i = 0 ; i<N ; i++){
+        for(int j = 0 ; j<N ; j++){
+            cout<< matrix[i][j]<<" " ;
+            }
+        cout<<"\n";
+        }
+}
+
+void inputMatrix(double data[][N]){
+    for(int i=0;i<N;i++){
+        cout<<"Row "<<i+1<<": ";
+        for(int j=0;j<N;j++) cin>> data[i][j];
+    }
+}
+
+void findLocalMax(const double old[][N], bool matrix[][N]){
+    for(int i=0;i==0;i++){
+        for(int j=0;j<N;j++) matrix[i][j] = 0;
+    }
+    for(int i=0;i<N;i++){
+        for(int j=0;j==0;j++) matrix[i][j] = 0;
+    }
+    for(int i=0;i<N;i++){
+        matrix[i][N-1] = 0;
+    }
+    for(int j=0;j<N;j++){
+        matrix[N-1][j] = 0;
+    }
+    for(int i=1;i<N-1;i++){
+        for(int j=1;j<N-1;j++){
+        if(old[i][j]>=old[i-1][j]&&old[i][j]>=old[i+1][j]&&old[i][j]>=old[i][j-1]&&old[i][j]>=old[i][j+1]){ 
+        matrix[i][j] = true;
+        }
+        else matrix[i][j] = false;
+        }
+    }
+}
